3767f46340edf37de6d8c2772de27af8b3fe5068
pomeeeeeeeene <170702078+pomeeeeeeeene@users.noreply.github.com>
2025-01-16 03:14:23 +0900

    Add necessary iOS project files for Xcode setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      // Firebaseの初期化
+      FirebaseApp.configure()
+      
+      // Flutterのプラグイン登録
+      GeneratedPluginRegistrant.register(with:self)
+      
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
