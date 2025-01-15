97324b1bb8dd163241f5c15d42ac0840f83beb57
pomeeeeeeeene <170702078+pomeeeeeeeene@users.noreply.github.com>
2025-01-16 06:52:13 +0900

    Added setting to initialize firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:temp_project/login_page.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        home: const LoginPage()),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const LoginPage(),
+    );
+  }
 }
