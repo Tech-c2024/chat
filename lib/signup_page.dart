7e66b84bd2b26372cdcd98dfd816c3a016e3c98a
pomeeeeeeeene <170702078+pomeeeeeeeene@users.noreply.github.com>
2025-01-20 16:36:50 +0900

    Add username input field to SignUpPage and handle account creation errors - Added CustomTextFormField for username input - Integrated error handling for account creation process - Linked username with user account in Firestore

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -14,22 +15,42 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _errorMessage = '';
 
   Future<void> _signUp() async {
+    _errorMessage = '';
     try {
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
+        email: emailController.text.trim(), // .text 入力フィールドの文字列を取得
+        password: passwordController.text.trim(), // trim() 前後の空白を削除
       );
+
+      String uid = userCredential.user!.uid; // user! nullではないことを保証
+      await FirebaseFirestore.instance.collection('users').doc(uid).set({
+        'username': usernameController.text.trim(),
+        'email': emailController.text.trim(),
+        'uid': uid,
+        'createdAt': Timestamp.now(),
+      });
+
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => LoginPage()),
       );
       print("アカウントの作成に成功, UID:${userCredential.user?.uid}");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        _errorMessage = "このメールアドレスはすでに使用されています。";
+      } else {
+        _errorMessage = e.message ?? "アカウントの作成に失敗しました。";
+      }
+      print("エラー: $_errorMessage");
     } catch (e) {
-      print("アカウントの作成に失敗: $e");
+      _errorMessage = "予期しないエラーが発生しました: $e";
+      print("エラー: $_errorMessage");
     }
   }
 
@@ -45,6 +66,11 @@ class _SignUpPageState extends State<SignUpPage> {
           ),
         ),
         SizedBox(height: 30),
+        CustomTextFormField(
+          controller: usernameController,
+          labelText: 'username',
+        ),
+        SizedBox(height: 20),
         CustomTextFormField(
           controller: emailController,
           labelText: 'E-mail',
